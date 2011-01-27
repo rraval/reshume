11b11d1e3f4cb26fde6f61eeebbfa25b3e5e7f2c
Ronuk Raval <ronuk.raval@gmail.com>
2011-01-27 17:06:01 -0500

    Began newline handling

--- reshume.js
+++ reshume.js
@@ -2,18 +2,24 @@ function Console() {
     // the table we'll be manipulating
     this.console = document.getElementById("console");
     // command buffer, in reverse chronological order
-    this.buffer = [];
+    this.buffer = [""];
     // current cursor position
     this.currentpos = 0;
     // the shell prompt
     this.prompt = '<span class="cyantext">[</span>%pwd<span class="cyantext">]</span>$ ';
     // current working directory
     this.pwd = "/resume";
-    // the state of the cursor, for blinking purposes
-    this.blackCursor = false;
-    
+
     // handle onKeyPress, which only fires events for normal keys
     this.normalKey = function(event) {
+        var keycode = this.getKeyCode(event);
+
+        switch(keycode) {
+            // skip the events handled by controlKey
+            case 8: case 13: case 37: case 39:
+                return;
+        }
+
         // FIXME: insert characters based on currentpos!
         this.buffer[0] += String.fromCharCode(this.getKeyCode(event));
         ++this.currentpos;
@@ -24,30 +30,40 @@ function Console() {
     // the events are only fired by onKeyDown
     this.controlKey = function(event) {
         switch(this.getKeyCode(event)) {
-        case 8: // backspace
-            if(this.currentpos > 0) {
-                this.buffer[0] = this.buffer[0].substr(0, this.buffer[0].length - 1);
-                --this.currentpos;
+            case 8: // backspace
+                if(this.currentpos > 0) {
+                    this.buffer[0] = this.buffer[0].substr(0, this.buffer[0].length - 1);
+                    --this.currentpos;
+                    this.update();
+                } else {
+                    this.beep();
+                }
+                break;
+            case 13:
+                // remove the cursor from the current command
+                var row = this.console.rows[this.console.rows.length - 1];
+                var prompt = this.prompt.replace("%pwd", this.pwd);
+                row.innerHTML = '<td><pre>' + prompt + this.buffer[0] + '</pre></td>';
+
+                this.console.insertRow(this.console.rows.length);
+                this.buffer.unshift("");
                 this.update();
-            } else {
-                this.beep();
-            }
-            break;
-        case 37: // left key
-            if(this.currentpos > 0) {
-                --this.currentpos;
-                this.update();
-            } else {
-                this.beep();
-            }
-            break;
-        case 39: // right key
-            if(this.currentpos < this.buffer[0].length) {
-                ++this.currentpos;
-                this.update();
-            } else {
-                this.beep();
-            }
+                break;
+            case 37: // left key
+                if(this.currentpos > 0) {
+                    --this.currentpos;
+                    this.update();
+                } else {
+                    this.beep();
+                }
+                break;
+            case 39: // right key
+                if(this.currentpos < this.buffer[0].length) {
+                    ++this.currentpos;
+                    this.update();
+                } else {
+                    this.beep();
+                }
         }
     };
 
@@ -55,6 +71,7 @@ function Console() {
         var row = this.console.rows[this.console.rows.length - 1];
         var prompt = this.prompt.replace("%pwd", this.pwd);
         var html;
+
         if(!this.buffer[0].charAt(this.currentpos)) {
             // cursor at end of line
             html = prompt + this.buffer[0] + '<span id="cursor">&nbsp;</span>';
@@ -62,16 +79,14 @@ function Console() {
             var early = this.buffer[0].substr(0, this.currentpos);
             var late = this.buffer[0].substr(this.currentpos + 1);
             html = prompt + early + '<span id="cursor">' +
-                this.buffer[0].charAt(this.currentpos) + '</span>' + late;
+                    this.buffer[0].charAt(this.currentpos) + '</span>' + late;
         }
+
         row.innerHTML = '<td><pre>' + html + '</pre></td>';
     };
-    
+
     this.newline = function() {
-        this.console.insertRow();
-        this.buffer.unshift("");
-        this.update();
-    };
+   };
 
     this.getKeyCode = function(event) {
         var keycode = event.keyCode; // IE or Webkit
@@ -98,10 +113,5 @@ function Console() {
         }
     };
 
-    this.newline();
-
-    /* blink the cursor
-        this.blinkCursor();
-        window.setInterval(this.blinkCursor, 500);
-     */
+    this.update();
 }
